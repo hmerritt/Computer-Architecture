89c2c90b15d29c8941a99e38f2fd1d3a57660962
Harry Merritt <harry@merritt.es>
2020-09-03 20:14:47 +0100

    complete day 1: get print8.ls8 running

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,13 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0b00000000] * 255
+        self.reg = [0] * 8
+        self.pc = 0
+        self.ir = 0
+        self.mar = 0
+        self.mdr = 0
+        self.fl = 0
 
     def load(self):
         """Load a program into memory."""
@@ -31,12 +37,19 @@ class CPU:
             address += 1
 
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +75,23 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            self.ir = self.ram[self.pc] # get current instruction using program counter
+
+            if self.ir == 0b00000001: # HALT
+                running = False
+
+            elif self.ir == 0b10000010: # LDI
+                self.reg[self.ram[self.pc + 1]] = self.ram[self.pc + 2]
+                self.pc += 2
+
+            elif self.ir == 0b01000111: # PRN R0
+                self.pc += 1
+                print(self.reg[self.ram[self.pc]])
+
+            else:
+                print("Unknown instruction")
+                sys.exit(1)
+
+            self.pc += 1 # increment program counter
