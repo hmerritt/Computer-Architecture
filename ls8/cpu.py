5ef0361648d8461ece08d71b1eec9d627a1d4e59
Harry Merritt <harry@merritt.es>
2020-09-06 22:57:59 +0100

    implement load() function to load an .ls8 from cli arguments

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,24 +17,24 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        filename = sys.argv[1]
         address = 0
+        try:
+            with open(filename) as f:
+                for line in f:
+                    split_line = line.split('#')
+                    code_value = split_line[0].strip()
 
-        # For now, we've just hardcoded a program:
+                    if code_value == '':
+                        continue
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    num = int(code_value, 2)
+                    self.ram[address] = num
+                    address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        except FileNotFoundError:
+            print(f"{filename} file not found")
+            sys.exit(2)
 
 
     def ram_read(self, address):
